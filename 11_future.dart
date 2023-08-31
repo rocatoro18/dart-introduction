f6dd884e8ca2c50a7e1243c42692e99396fcfd3b
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-08-31 16:36:38 -0700

    async

--- 11_future.dart
+++ 11_future.dart
@@ -1,16 +1,20 @@
-void main() {
+void main() async {
   print('Inicio del programa');
-  httpGet('').then((value) {
+
+  try {
+    final value = await httpGet('');
     print(value);
-  }).catchError((err) {
-    print('Error: $err');
-  });
+  } catch (err) {
+    print(err);
+  }
+
   print('Fin del programa');
 }
 
-Future<String> httpGet(String url) {
-  return Future.delayed(const Duration(seconds: 2), () {
-    throw 'Error en la peticion http';
-    //return 'Respues peticion http';
-  });
+Future<String> httpGet(String url) async {
+  await Future.delayed(const Duration(seconds: 1), () {});
+
+  throw 'Error en la peticion';
+
+  return 'Tenemos un valor de la petición';
 }
