749e7ac13109c4f93aff18a9e300825580f361ff
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-09-01 00:30:28 -0700

    streams

---
+++ 12_streams.dart
@@ -0,0 +1,13 @@
+// Un stream es un flujo de datos
+void main() {
+  emitNumbers().listen((value) {
+    print('Stream value: $value');
+  });
+}
+
+Stream<int> emitNumbers() {
+  return Stream.periodic(const Duration(seconds: 1), (value) {
+    //print('desde periodic $value');
+    return value;
+  }).take(5);
+}
